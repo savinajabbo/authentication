6323f2563cc3af6335a2d779d936b35cec971aa2
Savina Jabbo <savina.jabbo@gmail.com>
2024-03-15 20:36:19 -0700

    updated files

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native';
+import Logo from '../../../assets/images/silicon-valley.jpeg';
+
+const SignInScreen = () => {
+    const {height} = useWindowDimensions();
+    return (
+        <View style={styles.root}>
+            <Image source={Logo} style={[styles.logo, {height: height * 0.3}]} resizeMode='contain'/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    logo: {
+        width: '70%',
+        maxWidth: 300,
+        maxHeight: 200,
+    }
+})
+
+export default SignInScreen
